d46bcf6c4aae39ca84050b107149c421a99b2090
ayahuasca09 <ayahuascaa09@gmail.com>
2024-06-05 11:18:58 +0800

    文件测试

--- module/waapi/waapi_test.py
+++ module/waapi/waapi_test.py
@@ -2,11 +2,37 @@ from waapi import WaapiClient, CannotConnectToWaapiException
 from pprint import pprint
 
 with WaapiClient() as client:
-    args = {
-        "soundbanks": [
-            {"name": "ababa"}
-        ],
-        "writeToDisk": True
-    }
+    """soundbank生成测试"""
+    # args = {
+    #     "soundbanks": [
+    #         {"name": "ababa"}
+    #     ],
+    #     "writeToDisk": True
+    # }
+    #
+    # client.call("ak.wwise.core.soundbank.generate", args)
 
-    client.call("ak.wwise.core.soundbank.generate", args)
+    """基本功能测试"""
+    # args = {
+    #     'waql': '$ "\\Events" select descendants where type = "Event" '
+    # }
+    # options = {
+    #     'return': ["parent", "name"]
+    # }
+    # # 存储了所有Event的单元结构
+    # wwise_info_list = client.call("ak.wwise.core.object.get", args, options = options)['return']
+    # pprint(wwise_info_list)
+
+    """获取选中对象的打包流媒体的路径"""
+    # objects_list = client.call("ak.wwise.ui.getSelectedObjects")['objects']
+    # obj_id = objects_list[0]['id']
+    # print(obj_id)
+    # args = {
+    #     'waql': '"%s"' % obj_id
+    # }
+    # options = {
+    #     'return': ['name','convertedWemFilePath']
+    # }
+    # # 存储了所有Event的单元结构
+    # wwise_info_list = client.call("ak.wwise.core.object.get", args, options=options)['return']
+    # pprint(wwise_info_list)
--- test/test.py
+++ test/test.py
@@ -1,1 +1,11 @@
-print("aa")
+import os
+from os.path import abspath, dirname
+
+"""输出测试"""
+# print("aa")
+
+"""获取当前.py所在路径测试"""
+# 绝对路径
+# print(abspath(__file__))
+# 文件所在目录
+print(dirname(abspath(__file__)))
