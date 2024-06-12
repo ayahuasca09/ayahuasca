1ad4de143486a62430a252b00c73ad561696f5fb
ayahuasca09 <ayahuascaa09@gmail.com>
2024-06-12 23:03:29 +0800

    音频资源导入检测——命名规范细则补充

--- module/waapi/waapi_音频资源导入自动化/waapi_音频资源导入自动化.py
+++ module/waapi/waapi_音频资源导入自动化/waapi_音频资源导入自动化.py
@@ -18,7 +18,7 @@ json_name = '导入规则.json'
 json_path = os.path.join(py_path, json_name)
 
 # 打开json文件并转为python字典
-with open(json_path, 'r') as jsfile:
+with open(json_path, 'r', encoding='UTF-8') as jsfile:
     js_dict = json.load(jsfile)
 
 """*****************功能检测区******************"""
@@ -37,10 +37,10 @@ def check_is_chinese(string):
 """通过正则表达式检查"""
 
 
-def check_by_re(pattern, name, log):
+def check_by_re(pattern, name):
     result = re.search(pattern, name)
     if result == None:
-        print((cell_sound.value + log))
+        print((cell_sound.value + "：请检查" + pattern + "是否拼写错误或未添加到列表中"))
     else:
         new_name = name.replace(result.group(), "")
         return new_name
@@ -109,28 +109,121 @@ def check_by_json_by_re(system_dict):
 def check_by_char(name):
     module_dict = js_dict['Char']['module']
     flag = 0
+    is_mov = False
     for module in module_dict:
         # 模块层查询
         if module + "_" in name:
             name = name.replace(module + "_", "")
             # 角色名层查询
-            name = check_by_re(js_dict['Char']['name'] + "_", name,
-                               "：角色名称（如C01）不正确，请检查")
+            name = check_by_re(js_dict['Char']['name'] + "_", name)
             if name != None:
-                # 技能层查询
-                name = check_by_re(module_dict[module]['property'] + "_*", name,
-                                   "：技能名称等不在通用列表中，请查看是否拼写错误或需要添加列表")
+                if "Mov_" in name:
+                    is_mov = True
+                # property
+                name = check_by_re(module_dict[module]['property'] + "_*", name)
                 # 长度限制查询
                 if name != None:
-                    if len(name) > js_dict['Char']['length']:
-                        print(
-                            cell_sound.value + "：状态/描述/修饰后缀过长，请考虑精简字符")
+                    check_by_str_length(name, js_dict['Char']['length'])
+                # Mov层加查询
+                if is_mov == True:
+                    name = check_by_re(module_dict[module]['property2'] + "_*", name)
+                    if name != None:
+                        name = check_by_re(module_dict[module]['property3'] + "_*", name)
 
             flag = 1
             break
 
     if flag == 0:
         print(cell_sound.value + "：Char的模块（如Skill，Foley等）有误，请检查是否添加模块名称或是否拼写有误")
+
+
+"""Mon类型检查"""
+
+
+def check_by_mon(name):
+    if "Mob_" in name:
+        name = name.replace("Mob_", "")
+        check_by_mob(name)
+    elif "Boss_" in name:
+        name = name.replace("Boss_", "")
+        check_by_boss(name)
+    else:
+        print(cell_sound.value + "：Boss、Mob拼写有误或漏写，请检查")
+
+
+"""Boss类型检查"""
+
+
+def check_by_boss(name):
+    module_dict = js_dict['Mon']['Boss']['module']
+    flag = 0
+    for module in module_dict:
+        # 模块层查询
+        if module + "_" in name:
+            name = name.replace(module + "_", "")
+            # Boss名层查询
+            name = check_by_re(js_dict['Mon']['Boss']['name'] + "_", name)
+            if name != None:
+                # 技能层查询
+                name = check_by_re(module_dict[module]['property'] + "_*", name)
+                # 长度限制查询
+                if name != None:
+                    check_by_str_length(name, js_dict['Mon']['Boss']['length'])
+
+            flag = 1
+            break
+
+    if flag == 0:
+        print(cell_sound.value + "：Boss的模块（如Body，Part等）有误，请检查是否添加模块名称或是否拼写有误")
+
+
+"""Mob类型检查"""
+
+
+def check_by_mob(name):
+    module_dict = js_dict['Mon']['Mob']['module']
+    flag = 0
+    for module in module_dict:
+        # 模块层查询
+        if module + "_" in name:
+            name = name.replace(module + "_", "")
+            # 小怪名层查询
+            name = check_by_re(js_dict['Mon']['Mob']['name'] + "_", name)
+            if name != None:
+                # 技能层查询
+                name = check_by_re(module_dict[module]['property'] + "_*", name)
+                # 长度限制查询
+                if name != None:
+                    check_by_str_length(name, js_dict['Mon']['Mob']['length'])
+            flag = 1
+            break
+
+    if flag == 0:
+        print(cell_sound.value + "：Mob的模块（如Skill，Foley等）有误，请检查是否添加模块名称或是否拼写有误")
+
+
+def check_by_sys(name):
+    module_dict = js_dict['Sys']['module']
+    flag = 0
+    for module in module_dict:
+        # 模块层查询
+        if module + "_" in name:
+            name = name.replace(module + "_", "")
+            # Show类查询
+            if module == "Show":
+                if name != None:
+                    # 角色名称
+                    name = check_by_re(module_dict[module]['property'] + "_", name)
+                    if name != None:
+                        # 皮肤名称
+                        name = check_by_re(module_dict[module]['property2'] + "_", name)
+                        if name != None:
+                            # 动作名称
+                            name = check_by_re(module_dict[module]['property3'] + "_", name)
+        flag = 1
+        break
+    if flag == 0:
+        print(cell_sound.value + "：Sys的模块（如Show等）有误，请检查是否添加模块名称或是否拼写有误")
 
 
 """获取一级系统名走不同的检测方式"""
@@ -146,11 +239,11 @@ def check_first_system_name(name):
     elif word_list[0] == "Imp":
         return name.replace("Imp_", "")
     elif word_list[0] == "Mon":
-        return name.replace("Mon_", "")
+        check_by_mon(name.replace("Mon_", ""))
     elif word_list[0] == "Mus":
         return name.replace("Mus_", "")
     elif word_list[0] == "Sys":
-        return name.replace("Sys_", "")
+        check_by_sys(name.replace("Sys_", ""))
     elif word_list[0] == "VO":
         return name.replace("VO_", "")
     else:
@@ -171,9 +264,22 @@ def check_LP_in_last():
             new_name = re.sub(r"_LP$", "", name)
             return new_name
         else:
-            print(name + "：_LP应放在最末尾")
+            print(cell_sound.value + "：_LP应放在最末尾")
     else:
         return name
+
+
+"""检查是否使用通用词汇"""
+
+
+def check_by_com_word(name):
+    com_word_dict = js_dict['Gen_Word']
+    for key in com_word_dict:
+        for value in com_word_dict[key]:
+            if value in name:
+                print(cell_sound.value + "：" + value + "应改为通用词" + key)
+                if "Medium" in name:
+                    print("PS：Mid表体积/重量，Med表距离")
 
 
 """*****************主程序处理******************"""
@@ -214,10 +320,11 @@ for i in file_name_list:
                                     """判定_的每个都不能超过10个"""
                                     check_by_str_length(word, 10)
                                     """判定_的每个开头必须大写"""
-                                    # 只需要检查第一个字符，因为istitle会导致检查字符串只能首字母大写
-                                    if word[0].istitle() == False and word[0].isdigit() == False:
-                                        is_title = False
-                                        break
+                                    if len(word) > 0:
+                                        # 只需要检查第一个字符，因为istitle会导致检查字符串只能首字母大写
+                                        if word[0].istitle() == False and word[0].isdigit() == False:
+                                            is_title = False
+                                            break
                                 if is_title == False:
                                     print(name + "：通过”_“分隔的每个单词开头都需要大写")
 
@@ -228,3 +335,6 @@ for i in file_name_list:
                                 # 检查一级系统并索引到不同的检测方式
                                 check_first_system_name(name)
                                 # print(name)
+
+                                # 通用词汇检查
+                                check_by_com_word(name)
--- test/test.py
+++ test/test.py
@@ -6,18 +6,39 @@ import sys
 # print("aa")
 
 """获取当前.py所在路径测试"""
-# # 绝对路径
-# print(abspath(__file__))
-# 文件所在目录
-py_path = dirname(abspath(__file__))
-# 文件拼接
-file_name = 'test.txt'
-file_path = os.path.join(py_path, file_name)
-with open(file_path, 'r', encoding='utf-8') as file:
-    content = file.read()
-print(content)
-os.system(file_path)
+# # # 绝对路径
+# # print(abspath(__file__))
+# # 文件所在目录
+# py_path = dirname(abspath(__file__))
+# # 文件拼接
+# file_name = 'test.txt'
+# file_path = os.path.join(py_path, file_name)
+# with open(file_path, 'r', encoding='utf-8') as file:
+#     content = file.read()
+# print(content)
+# os.system(file_path)
 
 """打开文件操作测试"""
 # os.system("F:/pppppy/SP/test/test.txt")
 
+"""查找值对应的键"""
+# args = {
+#     "Gen_Word": {
+#         "Lit": ["Light", "Small"],
+#         "Mid": ["Middle", "Medium"],
+#         "Hvy": ["Heavy", "Large", "Big"],
+#         "Far": ["Distant"],
+#         "Med": ["Medium"],
+#         "Cls": ["Close", "Near"],
+#         "VeryCls": ["VeryClose"],
+#         "LP": ["Lp", "Loop", "lp"],
+#         "Grd": ["Ground"],
+#         "Death": ["Die", "Dying"],
+#         "Sucs": ["Succeed", "Success"],
+#         "Fail": ["False", "Lose", "Failure"]
+#     }
+# }
+# for key in args['Gen_Word']:
+#     for value in args['Gen_Word'][key]:
+#         if value == "Light":
+#             print(key)
