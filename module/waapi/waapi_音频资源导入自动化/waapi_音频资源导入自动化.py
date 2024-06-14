ec3572cd7275dff8917f6a0cc5f4dc8da5c353d9
ayahuasca09 <ayahuascaa09@gmail.com>
2024-06-14 17:03:27 +0800

    音频资源导入检测——随机容器可以根据分类创建

--- module/waapi/waapi_音频资源导入自动化/waapi_音频资源导入自动化.py
+++ module/waapi/waapi_音频资源导入自动化/waapi_音频资源导入自动化.py
@@ -286,6 +286,7 @@ def check_by_com_word(name):
 
 
 def check_first_system_name(name):
+    system_name = ""
     if word_list[0] == "Amb":
         return name.replace("Amb_", "")
     elif word_list[0] == "Cg":
@@ -293,7 +294,7 @@ def check_first_system_name(name):
     elif word_list[0] == "Char":
         # 如果检测通过
         if (check_by_char(name.replace("Char_", ""))) == True:
-            create_wwise_content(wwise_dict['Root'], cell_sound.value)
+            create_wwise_content(os.path.join(wwise_dict['Root'], "Char"), cell_sound.value)
     elif word_list[0] == "Imp":
         return name.replace("Imp_", "")
     elif word_list[0] == "Mon":
@@ -333,8 +334,27 @@ with WaapiClient() as client:
     """随机容器创建"""
 
 
-    def create_rnd_container(path, rnd_name):
+    # 数据获取
 
+    def find_rnd_parent(path, rnd_name):
+        # 所有Actor-Mixer获取
+        mixer_list, mixer_id = find_obj(
+            {'waql': ' "%s" select descendants,this where type = "ActorMixer" ' % path})
+        # pprint(mixer_container_list)
+        # [{'id': '{446650AE-60B4-40A0-8B14-3470DBBB83B0}', 'name': 'Amb', 'notes': ''},
+        #  {'id': '{E0A2685C-387A-4180-BB45-6E6C1559E7F8}', 'name': 'CG', 'notes': ''}]
+        # 存储符合的rnd的父级，最后找出最长最符合的
+        mixer_len = 0
+        rnd_parent = {}
+        for mixer_dict in mixer_list:
+            if mixer_dict['name'] in rnd_name:
+                if len(mixer_dict['name']) > mixer_len:
+                    mixer_len = len(mixer_dict['name'])
+                    rnd_parent = mixer_dict['id']
+        return rnd_parent
+
+
+    def create_rnd_container(path, rnd_name):
         # 查找该rnd是否已存在
         flag = 0
         rnd_container_list, rnd_id = find_obj(
@@ -349,30 +369,37 @@ with WaapiClient() as client:
             original_name = rnd_name
             # 去除随机容器数字
             rnd_name = re.sub(r"(_R\d{2,4})$", "", rnd_name)
-            # 创建的rnd属性
-            args = {
-                # 选择父级
-                "parent": path,
-                # 创建类型机名称
-                "type": "RandomSequenceContainer",
-                "name": rnd_name,
-                "@RandomOrSequence": 1,
-                "@NormalOrShuffle": 0,
-                "@RandomAvoidRepeatingCount": 3
-            }
-            rnd_container_object = client.call("ak.wwise.core.object.create", args)
-            pprint(rnd_name + "：RandomContainer创建")
+            # 查找所在路径
+            rnd_parent_id = find_rnd_parent(path, rnd_name)
+            if rnd_parent_id != None:
+                # 创建的rnd属性
+                args = {
+                    # 选择父级
+                    "parent": rnd_parent_id,
+                    # 创建类型机名称
+                    "type": "RandomSequenceContainer",
+                    "name": rnd_name,
+                    "@RandomOrSequence": 1,
+                    "@NormalOrShuffle": 0,
+                    "@RandomAvoidRepeatingCount": 3
+                }
+                rnd_container_object = client.call("ak.wwise.core.object.create", args)
+                pprint(rnd_name + "：RandomContainer创建")
 
 
     """媒体资源导入的总流程"""
 
 
     def create_wwise_content(path, rnd_name):
+
         # 随机容器创建
         create_rnd_container(path, rnd_name)
 
 
     """*****************主程序处理******************"""
+    # 撤销开始
+    client.call("ak.wwise.core.undo.beginGroup")
+
     # 提取规则：只提取xlsx文件
     for i in file_name_list:
         if ".xlsx" in i:
@@ -421,3 +448,8 @@ with WaapiClient() as client:
                                     # 检查一级系统并索引到不同的检测方式
                                     check_first_system_name(name)
                                     # print(name)
+    # 撤销结束
+    client.call("ak.wwise.core.undo.endGroup", displayName="rnd创建撤销")
+
+    # rnd创建撤销
+    # client.call("ak.wwise.core.undo.undo")
